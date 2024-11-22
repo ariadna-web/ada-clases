08a7325c6eddba6dccc8e2fd9b5a908380a7da0c
ariadna-web <ariadnagermano140@gmail.com>
2024-11-21 23:00:49 -0300

    modificando

--- clase 9/ejercicio2.js
+++ clase 9/ejercicio2.js
@@ -2,7 +2,7 @@
 const prompt = require("prompt-sync")({ sigint: true });
 const rango_minimo = 10;
 const rango_maximo = 100;
-let numero = parseFloat(prompt("ingresa tu numero"));
+let numero = parseFloat(prompt("ingresa tu numero: "));
 if (numero >= rango_minimo && numero <= rango_maximo) {
     console.log(`el numero ${numero} esta dentro del rango${rango_minimo}-${rango_maximo}`);
 } else {
