fdc8a912d7418fb756103ee1c98d1e1beba7170a
ariadna-web <ariadnagermano140@gmail.com>
2025-02-23 20:48:33 -0300

    clase 11

---
+++ BACK1/clase11/ejemplo.js
@@ -0,0 +1,8 @@
+const ejemplo1 = Path.basename//obtiene el nombre del archivo
+const ejemplo2 = path.dirname//obtiene el directorio de una ruta
+const ejemplo3 = path.extname //obtiene la extencion del archivo
+const ejemplo4 = path.join //unse segmentos de rutas de manera compatible con el sistema
+const ejemplo5 = path.resolve //resuelve una ruta absoluta
+const ejemplo6 = path.normalize //normaliza una ruta eliminando redundancias
+const ejemplo7 = path.parse //convierte una ruta en un objeto con propiedades(dir,base,ext,name)
+const ejemplo8 = path.format //convierte un objeto de ruta en una cadena de texto
